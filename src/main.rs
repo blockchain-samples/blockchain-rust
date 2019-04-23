3d84f720cdb0e5467ec512c262f0c695c9ae52c6
Pedro Mendonça <pedromdsmendonca@gmail.com>
2019-04-23 23:54:59 +0100

    Started implementing blocks and blockchain

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+#[derive(Clone)]
+struct Block {
+    data: u32,
+    previous_hash: String,
+    hash: String
+}
+
+impl Block {
+    fn to_string(&self) -> String {
+        format!("data {}", self.data)
+    }
+
+    fn calc_hash(&mut self) {
+        self.hash = "1".to_string()
+    }
+}
+
+#[derive(Clone)]
+struct Blockchain<'a> {
+    chain: &'a mut Vec<Block>
+}
+
+impl<'a> Blockchain<'a>{
+    fn add_block(&mut self, block: Block) {
+        self.chain.push(block)
+    }
+
+    fn print(&self){
+        for b in self.chain.iter_mut(){
+            println!("{}",b.to_string());
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut b = Block{
+        data: 1,
+        previous_hash: "".to_string(),
+        hash: "".to_string()
+    };
+
+    b.calc_hash();
+
+    println!("{}", b.to_string());
 }
